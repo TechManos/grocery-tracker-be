cdc78b74ea66b9dd00dd6a592564ec4a3625b299
yurii.fedak <yurii.fedak@workflows.sk>
2025-03-08 21:08:18 +0100

    Reworked concept of REST exposition without deleted entries: now the default functionality will be in place, but we will expose additional repository methods for our purposes, on customely defined patsh, for each softly deletable entity.

--- src/main/kotlin/com/merfolkstation/grocery/tracker/spring/model/repository/SoftDeleteRepository.kt
+++ src/main/kotlin/com/merfolkstation/grocery/tracker/spring/model/repository/SoftDeleteRepository.kt
@@ -5,25 +5,29 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.NoRepositoryBean
+import org.springframework.data.rest.core.annotation.RestResource
 import java.util.*
 
 @NoRepositoryBean
 interface SoftDeleteRepository<T, ID> : JpaRepository<T, ID> {
 
-    // Override default methods to filter deleted entities
+    @RestResource(path = "active", rel = "active")  // Custom path to avoid ambiguity
     @Query("SELECT e FROM #{#entityName} e WHERE e.deletedAt IS NULL")
-    override fun findAll(): List<T>
+    fun findActive(): List<T>
 
+    @RestResource(path = "active-paged", rel = "active-paged")  // Custom path for paged version
     @Query("SELECT e FROM #{#entityName} e WHERE e.deletedAt IS NULL")
-    override fun findAll(pageable: Pageable): Page<T>
+    fun findActive(pageable: Pageable): Page<T>
 
     @Query("SELECT e FROM #{#entityName} e WHERE e.id = :id AND e.deletedAt IS NULL")
     override fun findById(id: ID & Any): Optional<T>
 
     // Add methods to access deleted entities if needed
     @Query("SELECT e FROM #{#entityName} e WHERE e.deletedAt IS NOT NULL")
+    @RestResource(path = "deleted", rel = "deleted")
     fun findDeleted(): List<T>
 
     @Query("SELECT e FROM #{#entityName} e")
+    @RestResource(path = "all-including-deleted", rel = "all-including-deleted")
     fun findAllIncludingDeleted(): List<T>
 }
