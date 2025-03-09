729db8720b65459de4eb216d0022d409ce0d1cf7
yurii.fedak <yurii.fedak@workflows.sk>
2025-03-09 01:03:23 +0100

    Add jsr310 (java date/time)

--- src/main/kotlin/com/merfolkstation/grocery/tracker/spring/configuration/JacksonConfig.kt
+++ src/main/kotlin/com/merfolkstation/grocery/tracker/spring/configuration/JacksonConfig.kt
@@ -1,6 +1,7 @@
 package com.merfolkstation.grocery.tracker.spring.configuration
 
 import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
